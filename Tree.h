4ee11fbe664fa33752bb4caf2a9a2955349237d4
endwater <43136657+endwater@users.noreply.github.com>
2018-11-09 21:25:59 -0800

    Create Tree.h

---
+++ Tree.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Tree {
+public:
+	class Node {
+	public:
+		Node() : left(nullptr), right(nullptr), data(0) {}
+		Node(int val) : left(nullptr), right(nullptr), data(val) {}
+		int data;
+		Node *left, *right;
+	};
+
+	Tree() : count(0), root(nullptr) {}
+	bool insert(int newVal);
+	friend ostream& operator << (ostream& str, const Tree& tree);  // calls print and 
+																   // outputs count
+private:
+	void print(ostream& ostr, Node* curNode) const;  // recursive function
+	Node* root;
+	int count;
+};
